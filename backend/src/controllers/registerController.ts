05b42825c324e6391ea247f8fce5fae8ad557163
MuhammadFarouk12 <muhammad12fk@gmail.com>
2025-10-13 20:04:19 +0300

    Adding Zod Library, UserRequestSchema, refactoring the code, cleaner error handling

--- backend/src/controllers/registerController.ts
+++ backend/src/controllers/registerController.ts
@@ -1,40 +1,43 @@
 import { Request, Response, NextFunction } from "express";
 import { Prisma, PrismaClient } from "../../generated/prisma";
 import bcrypt from "bcryptjs"
-import { HTTPResponseStatus, JSONResponse } from "../utils/httpStatus";
+import { JSONResponse } from "../utils/httpStatus";
 import { JWT } from "../utils/createJWT";
-import jwt from "jsonwebtoken";
+import z, { ZodError } from "zod"
+
 const prisma = new PrismaClient()
 
-export function registerController(req: Request, res: Response, next: NextFunction){
-	async function addUserToDB(){
-		try{
-			const user = await prisma.users.create({
-				data: {
-					user_name: req.body.username.toString(),
-					user_password: bcrypt.hashSync(req.body.password.toString(), 10)
-				}	
-			})
-			const response: JSONResponse = {
-				status: "SUCCESS",
-				code: 200,
-				data: {user_id: user.user_id, user_name: user.user_name}
-			}
-			const userToken = JWT({user_name: user.user_name, user_id: user.user_id, iat: Date.now()})
-			res.cookie('token', userToken, {path: '/', sameSite: true})	
-			res.json(response)
+const RegisterRequest = z.object({
+	user_name: z.string(),
+	user_password: z.number().or(z.string()) 
+})
 
-		} catch(error){
-			if (error instanceof Prisma.PrismaClientKnownRequestError) {
-				const response: JSONResponse = {
-					status: "ERROR",
-					code: 400,
-					data: null,
-					message: error.message
-				}
-				res.status(response.code).json(response)
-			}
+async function createUser(req: Request){
+	const user = await prisma.users.create({
+		data: {
+			user_name: req.body.user_name.toString(),
+			user_password: bcrypt.hashSync(req.body.user_password.toString(), 10)
 		}
+	})
+	return user
+}
+
+export async function registerController(req: Request, res: Response, next: NextFunction){
+	let response: JSONResponse = { status: "ERROR", code: 500, data: null, message: "Internal Server Error At Register Controller" }
+	try {
+		RegisterRequest.parse(req.body)
+		const user =  await createUser(req)
+		const userToken = JWT({user_name: user.user_name, user_id: user.user_id, iat: Date.now()})
+		response = { status: "SUCCESS", code: 200, data: {user_id: user.user_id, user_name: user.user_name} }
+		res.cookie('token', userToken, {path: '/', sameSite: true})	
+	} catch (error) {
+		if(error instanceof ZodError){
+			 response = { status: "ERROR", code: 400, data: null, message: error.message }
+		}
+		else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				response = { status: "ERROR", code: 400, data: null, message: error.message }
+		}
+	} finally {
+		res.status(response.code).json(response)
 	}
-	addUserToDB()
 } 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import express, {Request, Response, NextFunction } from "express"
 import usersRoute from "./routes/usersRoute.ts"
 import { registerRoute } from "./routes/registerRoute.ts"
+// import { loginRoute } from "./routes/loginRoute.ts"
 import cookieParser from "cookie-parser"
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/v1/register", registerRoute)
-
+// app.use("/api/v1/login", loginRoute)
 app.listen(3000, () => {
 	console.log("SERVER IS RUNNING VIA PORT 3000")
 })
